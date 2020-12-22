7b0bf60c7b0c58c2b90cf474d39d66da22bf5d32
adarvin14 <amdarvin14@gmail.com>
2020-12-22 17:49:31 -0500

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph')
 
+hearts.forEach(heart => {
+  heart.addEventListener('click', (e) => {
+    mimicServerCall()
+    .then(resolve => {
+      if (heart.innerHTML == EMPTY_HEART) {
+        heart.innerHTML = FULL_HEART
+        heart.setAttribute('class', 'activated-heart')
+      } else {
+        heart.innerHTML = EMPTY_HEART
+        heart.setAttribute('class', '')
+      }
+    })
+    .catch(reject => {
+      const modal = document.querySelector('div#modal')
+      modal.setAttribute('class', '')
+      setTimeout(() => {modal.setAttribute('class', 'hidden')}, 5000)
+    })
+  })
+})
 
 
 
